b5fe2f35f4cdf6e05f45d7def7da9876521731e1
samcaspus <sandeepguptan1998@gmail.com>
2021-04-22 01:24:52 +0530

    color games

--- screens/Oxygen.js
+++ screens/Oxygen.js
@@ -1,23 +1,83 @@
-import * as React from 'react'
-import {View } from 'react-native';
-import { Button, Divider } from 'react-native-paper';
+import * as React from "react";
+import { View, ScrollView } from "react-native";
+import { Button, Divider, Card } from "react-native-paper";
+import { useState } from "react";
+import { StyleSheet, TouchableHighlight } from "react-native";
+import { Avatar, Title, Paragraph } from "react-native-paper";
+import { RadioButton, Text, TextInput, Chip } from "react-native-paper";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
-function Oxygen(){
+function Oxygen() {
+  const [bloodDonor, setBloodDonor] = React.useState(true);
+  const [bloodPatient, setBloodPatient] = React.useState(false);
+  const [value, setValue] = React.useState("0");
+  const [text, setText] = React.useState("");
+  const [date, setDate] = React.useState(new Date());
+  const [mode, setMode] = React.useState("date");
+  const [show, setShow] = React.useState(false);
 
-    return(
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-        <Button icon="water" mode="contained" onPress={() => console.log('Pressed')}>
-            Doner
-        </Button>
-        <Divider />
-        <Divider />
-        <Button icon="water" mode="contained" onPress={() => console.log('Pressed')}>
-            Patient
-        </Button>
+  const LeftContent = (props) => <Avatar.Icon {...props} icon="account" />;
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+    console.log(date);
+  };
+
+  return (
+    <View>
+      <ScrollView>
+        {bloodDonor === false && bloodPatient === false && (
+          <>
+            <Card>
+              <Card.Title
+                title="Card Title"
+                subtitle="Card Subtitle"
+                left={LeftContent}
+              />
+              <Card.Content>
+                <Title>Card title</Title>
+                <Paragraph>Card content</Paragraph>
+              </Card.Content>
+              <TouchableHighlight
+                onPress={() => {
+                  alert("hello");
+                }}
+              >
+                <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+              </TouchableHighlight>
+              <Card.Actions>
+                <Button>Accept</Button>
+                <Button icon="download"></Button>
+              </Card.Actions>
+            </Card>
+          </>
+        )}
+
+        {bloodDonor === true && (
+          <>
+            <View>
+              <TextInput
+                label={"Enter Message (Max char 160)"}
+                maxLength={160}
+              ></TextInput>
+              <Button mode="contained">Search</Button>
+            </View>
+          </>
+        )}
+      </ScrollView>
     </View>
-    )
-
+  );
 }
-
 
 export default Oxygen;
