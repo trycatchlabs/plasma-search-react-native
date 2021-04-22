abdbdbbdf10b32f0245c989509777c8f44200635
samcaspus <sandeepguptan1998@gmail.com>
2021-04-22 18:23:14 +0530

    redux integration in progress

--- screens/Home.js
+++ screens/Home.js
@@ -1,23 +1,34 @@
-import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { BottomNavigation, Text } from 'react-native-paper';
-import BottomNav from '../components/BottomNav';
-import Header from '../components/Header';
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { BottomNavigation, Text } from "react-native-paper";
+import BottomNav from "../components/BottomNav";
+import Header from "../components/Header";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Blood from "./Blood";
+import { useState } from "react";
+import Login from "./Login";
+import Register from "./Register";
+
+const Stack = createStackNavigator();
 
 function Home() {
-
-    return (
-
-        <>
-            <Header/>
-            <BottomNav/>
-        </>
-
-
-    );
+  const [signedIn, setSignIn] = useState(false);
+  return (
+    <>
+      <Header />
+      {signedIn ? (
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={Login}></Stack.Screen>
+            <Stack.Screen name="Register" component={Register}></Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      ) : (
+        <BottomNav />
+      )}
+    </>
+  );
 }
 
-
-
-
-export default Home
+export default Home;
