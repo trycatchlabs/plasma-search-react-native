a9fd57f01436abb3cdec42d4bab727c10c9721ab
samcaspus <sandeepguptan1998@gmail.com>
2021-04-27 22:43:59 +0530

    base features of blood

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,14 +1,47 @@
 import * as React from "react";
-import { ScrollView } from "react-native";
-import { Button, Divider } from "react-native-paper";
+import { Alert, ScrollView } from "react-native";
+import { Button, Card, Divider, Paragraph, Title } from "react-native-paper";
 import { connect } from "react-redux";
+import { Logout } from "../actions/AuthenticationActions";
+import { userLogout, userProfileInformation } from "../Api/ApiActions";
 
 function Profile(props) {
+  const { AuthenticationReducer, dispatch } = props;
+
+  const [resp, setResponse] = React.useState({});
+  React.useEffect(async () => {
+    let response = await userProfileInformation(
+      AuthenticationReducer.mobilenumber
+    );
+    setResponse(response);
+  }, []);
+
   return (
     <ScrollView>
-      <Divider />
-      <Divider />
-      <Divider />
+      <Card>
+        <Card.Content>
+          <Title>Name: {resp.name}</Title>
+          <Title>Email: {resp.email}</Title>
+          <Title>Gender: {resp.gender === 1 ? "Male" : "Female"}</Title>
+          <Title>Location: {resp.location} </Title>
+          <Title>Mobile number: {resp.mobileNumber}</Title>
+          <Title>Registerd On: {resp.registerdOn}</Title>
+          <Title>Age: {resp.age}</Title>
+        </Card.Content>
+        <Card.Actions>
+          <Button
+            mode="contained"
+            onPress={() => {
+              userLogout(AuthenticationReducer.mobilenumber);
+              dispatch(Logout());
+              Alert.alert("Logout Inetiated relaunch app to login again");
+            }}
+            style={{ width: "100%" }}
+          >
+            Logout
+          </Button>
+        </Card.Actions>
+      </Card>
     </ScrollView>
   );
 }
